a9dd30d21fdc90a5537a5bbc57e709c559af46c8
vladimir <74048469+sashimiv@users.noreply.github.com>
2023-05-03 18:08:45 +0300

    Update main.py
    уменьшен таймаут

--- main.py
+++ main.py
@@ -8,14 +8,13 @@ service = input("Выберите сервис(google/yandex/yahoo) ")
 if service == "google":
     url = "https://www.google.com/search?q=" + urlgets
     webbrowser.open(url, new=2, autoraise=True) 
-    time.sleep(20)   
+    time.sleep(5)   
 else:
     if service == "yahoo":
         url = "https://search.yahoo.com/search?p=" + urlgets
         webbrowser.open(url, new=2, autoraise=True)
-        time.sleep(20)
+        time.sleep(5)
     else:
         url = "https://yandex.ru/search/?lr=10735&text=" + urlgets
         webbrowser.open(url, new=2, autoraise=True)
-        time.sleep(20)
-#t.me/sashimiv спать
+        time.sleep(5)
