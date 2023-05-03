fedbd711279c758a2e0fe3d6d7dc127d777a5d4b
vladimir <74048469+sashimiv@users.noreply.github.com>
2023-05-03 18:10:33 +0300

    Update main.py
    немного уменьшил код сменив if на elif, а ещё теперь в линух с кедами не выдает ошибку если запуск в консоли

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ import time
 print("t.me/sashimiv")
 print()
 urlgets = input("Введите запрос: ")
-service = input("Выберите сервис(google/yandex/yahoo) ")
+service = input("Выберите сервис(google/yandex/yahoo): ")
 if service == "google":
     url = "https://www.google.com/search?q=" + urlgets
     webbrowser.open(url, new=2, autoraise=True) 
     time.sleep(5)   
+elif service == "yahoo":
+    url = "https://search.yahoo.com/search?p=" + urlgets
+    webbrowser.open(url, new=2, autoraise=True)
+    time.sleep(5)
 else:
-    if service == "yahoo":
-        url = "https://search.yahoo.com/search?p=" + urlgets
-        webbrowser.open(url, new=2, autoraise=True)
-        time.sleep(5)
-    else:
-        url = "https://yandex.ru/search/?lr=10735&text=" + urlgets
-        webbrowser.open(url, new=2, autoraise=True)
-        time.sleep(5)
+    url = "https://yandex.ru/search/?lr=10735&text=" + urlgets
+    webbrowser.open(url, new=2, autoraise=True)
+    time.sleep(5)
