6eedb818849f1e703e47a440cd15a5ff1c0f9042
vladimir <74048469+sashimiv@users.noreply.github.com>
2023-05-03 18:04:33 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import webbrowser
+from googlesearch import search
+import time
+print("t.me/sashimiv")
+print()
+urlgets = input("Введите запрос: ")
+service = input("Выберите сервис(google/yandex/yahoo) ")
+if service == "google":
+    url = "https://www.google.com/search?q=" + urlgets
+    webbrowser.open(url, new=2, autoraise=True) 
+    time.sleep(20)   
+else:
+    if service == "yahoo":
+        url = "https://search.yahoo.com/search?p=" + urlgets
+        webbrowser.open(url, new=2, autoraise=True)
+        time.sleep(20)
+    else:
+        url = "https://yandex.ru/search/?lr=10735&text=" + urlgets
+        webbrowser.open(url, new=2, autoraise=True)
+        time.sleep(20)
+#t.me/sashimiv спать
