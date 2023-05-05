67cab8a49cdcda47365434662b551b6dad1969d4
Vladimir Zolototrubov <74048469+sashimiv@users.noreply.github.com>
2023-05-05 21:23:54 +0300

    Update searchpro.py

--- searchpro.py
+++ searchpro.py
@@ -1,6 +1,6 @@
 konsolev = "0.0.6" 
 guiv = "1.0.5"
-searchpro = "0.0.3"
+searchpro = "0.0.2"
 userlang = input("select you lang(ru/en)")
 en = "avalibe services"
 ru = "Доступные сервисы: "
@@ -10,6 +10,5 @@ bing = "https://www.bing.com/search?q="
 duckduckgo = "https://duckduckgo.com/?q="
 yahoo = "https://search.yahoo.com/search?p="
 google = "https://www.google.com/search?q="
+url = input("Введите вашу ссылку")
 
-url = input("")
-print(servicesall)
