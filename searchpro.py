fa96bac86e379f0b768fb9de0cdd450f0b773e4f
Vladimir Zolototrubov <74048469+sashimiv@users.noreply.github.com>
2023-05-05 21:23:21 +0300

    Create searchpro.py

---
+++ searchpro.py
@@ -0,0 +1,15 @@
+konsolev = "0.0.6" 
+guiv = "1.0.5"
+searchpro = "0.0.3"
+userlang = input("select you lang(ru/en)")
+en = "avalibe services"
+ru = "Доступные сервисы: "
+servicesall = ru + "Bing, " + "DuckDuckGo, " + "Yahoo, " + "Google"
+
+bing = "https://www.bing.com/search?q="
+duckduckgo = "https://duckduckgo.com/?q="
+yahoo = "https://search.yahoo.com/search?p="
+google = "https://www.google.com/search?q="
+
+url = input("")
+print(servicesall)
