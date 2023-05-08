971852ba2354bb8275de5ca2e0dbce177a9acec3
Vladimir Zolototrubov <74048469+sashimiv@users.noreply.github.com>
2023-05-09 00:25:45 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,33 +1,34 @@
 import webbrowser
-import searchpro
+import time
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QRadioButton, QPushButton, QVBoxLayout, QHBoxLayout
 
 # импорт модуля searchpro
 # обязательно положите файл searchpro.py в ту же папку, что и этот файл
+import searchpro
 
-import time
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QRadioButton, QPushButton, QVBoxLayout, QHBoxLayout
 def open_browser(url):
     webbrowser.open(url, new=2, autoraise=True)
     time.sleep(5)
+    
 def search_query():
     urlgets = query_entry.text()
     service = service_var
     if service == "google":
-        url = google + urlgets
-        open_browser(url)
+        url = searchpro.google + urlgets
     elif service == "yahoo":
-        url = yahoo + urlgets
-        open_browser(url)
+        url = searchpro.yahoo + urlgets
     elif service == "duckduckgo":
-        url = duckduckgo + urlgets
-        open_browser(url)
+        url = searchpro.duckduckgo + urlgets
     else:
-        url = bing + urlgets
-        open_browser(url)
+        url = searchpro.bing + urlgets
+    open_browser(url)
+
 
 app = QApplication([])
+
 window = QWidget()
 window.setWindowTitle("search by sashimiv")
+
 query_label = QLabel("Введите запрос:")
 query_entry = QLineEdit()
 service_label = QLabel("Выберите сервис:")
@@ -36,6 +37,7 @@ yahoo_rb = QRadioButton("Yahoo")
 duckduckgo_rb = QRadioButton("DuckDuckGo")
 bing_rb = QRadioButton("Bing")
 search_button = QPushButton("Поиск в ❤️internet❤️")
+
 layout = QVBoxLayout()
 layout.addWidget(query_label)
 layout.addWidget(query_entry)
@@ -44,16 +46,16 @@ layout.addWidget(google_rb)
 layout.addWidget(yahoo_rb)
 layout.addWidget(duckduckgo_rb)
 layout.addWidget(bing_rb)
+
 button_layout = QHBoxLayout()
 button_layout.addWidget(search_button)
 layout.addLayout(button_layout)
+
 window.setLayout(layout)
+
 google_rb.setChecked(True)
 service_var = "google"
-google_rb.toggled.connect(lambda: service_radio_button(google_rb))
-yahoo_rb.toggled.connect(lambda: service_radio_button(yahoo_rb))
-duckduckgo_rb.toggled.connect(lambda: service_radio_button(duckduckgo_rb))
-bing_rb.toggled.connect(lambda: service_radio_button(bing_rb))
+
 def service_radio_button(rb):
     global service_var
     if rb.text() == "Google":
@@ -64,6 +66,13 @@ def service_radio_button(rb):
         service_var = "duckduckgo"
     else:
         service_var = ""
+        
+google_rb.toggled.connect(lambda: service_radio_button(google_rb))
+yahoo_rb.toggled.connect(lambda: service_radio_button(yahoo_rb))
+duckduckgo_rb.toggled.connect(lambda: service_radio_button(duckduckgo_rb))
+bing_rb.toggled.connect(lambda: service_radio_button(bing_rb))
 search_button.clicked.connect(search_query)
+
 window.show()
+
 app.exec_()
